5672e7b97172440d922691aa6f76d7ffb27438d2
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2024-07-19 05:45:25 -0500

    fixed app.use

--- Project AtmosphericX/index.js
+++ Project AtmosphericX/index.js
@@ -45,7 +45,7 @@ alreadyQuerying = false;
 alreadyQueryingDiscord = false;
 
 
-express = require("express")
+express = require("express");
 cryptography = require('crypto');
 fs = require('fs');
 path = require('path');
@@ -66,9 +66,12 @@ return new Promise(async (resolve, reject) => {
     console.log(ascii);
     configurations = await toolsConstructor.env('env');
     const app = express();
-    if (configurations['API_ACCESS'] != "[*]") {
-        let allowed = JSON.parse(configurations['API_ACCESS'])
-        app.use((req, res, next) => {
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
+        if (configurations['API_ACCESS'] != "[*]") {
+            let allowed = JSON.parse(configurations['API_ACCESS']);
             let ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress).replace('::ffff:', '');
             if (!allowed.includes(ip)) {
                 res.statusCode = HTTP_FORBIDDEN;
@@ -76,9 +79,45 @@ return new Promise(async (resolve, reject) => {
                 res.end(JSON.stringify({ error: 'Forbidden', message: 'Your address is not allowed to access this API.' }));
                 return;
             }
-            next();
-        });
-    }
+        }
+        if (req.url.includes('dashboard')) {
+            if (!loginAuthorization.includes(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
+                endpointConstructor.loginRedirect(req, res);
+                return;
+            }
+        }
+        if (req.url.includes('.')) {
+            let file_path = path.resolve('./www' + req.url);
+            let file_extension = path.extname(file_path);
+            if (fs.existsSync(file_path)) {
+                res.statusCode = HTTP_OK;
+                res.setHeader('Content-Type', file_extensions[file_extension]);
+                res.end(fs.readFileSync(file_path));
+            } else {
+                endpointConstructor.errorRedirect(req, res);
+                return;
+            }
+        } else {
+            if (req.url.includes('/api') || req.url == `/`) { next(); return; }
+            let file_path = path.resolve('./www' + req.url + '.html');
+            if (fs.existsSync(file_path)) {
+                res.statusCode = HTTP_OK;
+                res.setHeader('Content-Type', file_extensions['.html']);
+                res.end(fs.readFileSync(file_path));
+            } else {
+                endpointConstructor.errorRedirect(req, res);
+                return;
+            }
+        }
+        next();
+    });
+    app.get('/', (req, res) => {
+        if (!loginAuthorization.includes(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
+            endpointConstructor.loginRedirect(req, res);
+            return;
+        }
+        endpointConstructor.redirectDashboard(req, res);
+    });
     app.get('/api/alerts', (req, res) => {
         res.statusCode = HTTP_OK;
         res.setHeader('Content-Type', 'application/json');
@@ -153,61 +192,23 @@ return new Promise(async (resolve, reject) => {
     app.post('/api/logout', (req, res) => {
         endpointConstructor.requestLogoutPost(req, res, req.headers['x-forwarded-for'] || req.connection.remoteAddress);
     });
-    app.get('/', (req, res) => {
-        if (!loginAuthorization.includes(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
-            endpointConstructor.loginRedirect(req, res);
-            return;
-        }
-        endpointConstructor.redirectDashboard(req, res);
-    });
-    app.use((req, res, next) => {
-        if (req.url.includes('dashboard')) {
-            if (!loginAuthorization.includes(req.headers['x-forwarded-for'] || req.connection.remoteAddress)) {
-                endpointConstructor.loginRedirect(req, res);
-                return;
-            }
-        }
-        if (req.url.includes('.')) {
-            let file_path = path.resolve('./www' + req.url);
-            let file_extension = path.extname(file_path);
-            if (fs.existsSync(file_path)) {
-                res.statusCode = HTTP_OK;
-                res.setHeader('Content-Type', file_extensions[file_extension]);
-                res.end(fs.readFileSync(file_path));
-            } else {
-                endpointConstructor.errorRedirect(req, res);
-                return;
-            }
-        } else {
-            let file_path = path.resolve('./www' + req.url + '.html');
-            if (fs.existsSync(file_path)) {
-                res.statusCode = HTTP_OK;
-                res.setHeader('Content-Type', file_extensions['.html']);
-                res.end(fs.readFileSync(file_path));
-            } else {
-                endpointConstructor.errorRedirect(req, res);
-                return;
-            }
-        }
-        next();
-    });
     if (configurations['HTTPS'].toLowerCase() == 'true') {
         if (!fs.existsSync('./cert/generated.key') || !fs.existsSync('./cert/generated.crt')) {
             toolsConstructor.log('[Error] SSL Certificate and Key not found. Please run the generator script to generate the SSL certificate and key.\n\t\t\t\t\t\t   If you wish to run the server without SSL, please set HTTPS to false in the .env file.');
             process.exit(1);
         } else {
-            let ssl = {key: fs.readFileSync(`./cert/generated.key`),cert: fs.readFileSync(`./cert/generated.crt`)}
+            let ssl = { key: fs.readFileSync(`./cert/generated.key`), cert: fs.readFileSync(`./cert/generated.crt`) };
             var https = require('https');
             var http = require('http');
-            httpServer = http.createServer(app)
-            httpsServer = https.createServer(ssl, app)
-            httpServer.listen(configurations.PORT, () => {}).on('error', (err) => { toolsConstructor.log(`[Error] [WebServer] ${err}`); });
-            httpsServer.listen(configurations.SSL_PORT, () => {}).on('error', (err) => { toolsConstructor.log(`[Error] [WebServer] ${err}`); });
+            httpServer = http.createServer(app);
+            httpsServer = https.createServer(ssl, app);
+            httpServer.listen(configurations.PORT, () => { }).on('error', (err) => { toolsConstructor.log(`[Error] [WebServer] ${err}`); });
+            httpsServer.listen(configurations.SSL_PORT, () => { }).on('error', (err) => { toolsConstructor.log(`[Error] [WebServer] ${err}`); });
 
             toolsConstructor.log(`[Warning] Server is running in HTTPS/HTTP mode. Please ensure that your SSL certificate is valid and up-to-date.`);
             toolsConstructor.log(`Dasboard: https://${configurations.HOSTNAME}:${configurations.PORT} | http://${configurations.HOSTNAME}:${configurations.SSL_PORT}`);
         }
-    }else{
+    } else {
         toolsConstructor.log(`Dashboard: http://${configurations.HOSTNAME}:${configurations.PORT}`);
         app.listen(configurations.PORT, configurations.HOSTNAME, () => {
             toolsConstructor.log('Thank you for using AtmosphericX - Please stay safe and happy storm chasing!');
@@ -271,4 +272,4 @@ return new Promise(async (resolve, reject) => {
         bot_client.login(configurations.DISCORD_TOKEN);
     }
 });
-    
+
