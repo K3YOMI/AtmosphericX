4e9e628dae7084ed182a9384ac011f00ccd29ba3
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2024-07-30 02:19:24 -0500

    migrating

--- Project AtmosphericX/generator.sh
+++
@@ -1,3 +0,0 @@
-openssl genrsa -out cert/generated.key 2048
-openssl req -new -key cert/generated.key -out cert/generated.csr
-openssl x509 -req -days 365 -in cert/generated.csr -signkey cert/generated.key -out cert/generated.crt
