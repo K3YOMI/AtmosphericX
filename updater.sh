2d88087ccaa1d0b8cc670d95bfff34462237c80d
Kiyomi <54733885+K3YOMI@users.noreply.github.com>
2024-10-17 06:03:54 -0500

    Improved updaterw for better codeflow

--- updater.sh
+++ updater.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
-# This makes it easier to update/upgrade AtmosphericX Automatically
 
 KEEP_EXISTING=false
+
 
 echo "1. FULL UPGRADE"
 echo "  Discard local changes and pull new changes from the repository. (Caution: This will discard local changes such as certificates, cache, and configuration settings.)"
 echo "2. PARTIAL UPGRADE"
 echo "  Keep existing certificates, cache, and configuration settings. (Recommended for most users.)"
+echo "3. DEPENDENCY UPGRADE"
+echo "  Install dependencies (npm install)."
+echo "4. INSTALL (GIT)"
+echo "  Clone the repository from GitHub."
+
 
 read -p "Enter your choice (1 or 2): " choice
 if [[ $choice == 2 ]]; then
     KEEP_EXISTING=true
+fi
+
+if [[ $choice == 3 ]]; then
+    cd AtmosphericX
+    npm install .
+    exit 0
+fi
+
+if [[ $choice == 4 ]]; then
+    git clone https://github.com/k3yomi/AtmosphericX
+    exit 0
 fi
 
 if [[ $KEEP_EXISTING == false ]]; then
